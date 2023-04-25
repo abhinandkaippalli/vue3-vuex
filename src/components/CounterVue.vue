89cca5a8af22d6abcca5f7448c7b1d3c0af68020
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-25 11:50:24 +0530

    CounterVue componentpage designed

--- src/components/CounterVue.vue
+++ src/components/CounterVue.vue
@@ -1,9 +1,31 @@
 <template>
-    <div>
-        <p>Couter</p>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <p class="h3 text-success">Counter</p>
+            <p class="fst-italic">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima minus magnam mollitia accusamus, quis, atque consectetur cumque magni ipsa vero quibusdam assumenda officia error autem distinctio ad! Quod, quaerat dolore.</p>
+        </div>
     </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card shadow-lg">
+                <div class="card-body">
+                    <p class="h3 dispaly-3">9</p>
+                    <button class="btn btn-success m-1">Increment</button>
+                    <button class="btn btn-warning m-1">Decrement</button>
+                    <button class="btn btn-danger m-1">Increment By</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 <script>
-
+export default {
+    name: "CounterVue",
+    
+}
 </script>
 <style></style>
