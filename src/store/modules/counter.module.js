4727966cdbae674a45decc61e3f91faa3444d9ae
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-26 10:51:33 +0530

    mutations created

--- src/store/modules/counter.module.js
+++ src/store/modules/counter.module.js
@@ -6,6 +6,15 @@ export default {
         }
     },
     mutations: {
+        INCR_COUNTER: function (state) {
+            state.counter.count = state.counter.count + 1;
+        },
+        DECR_COUNTER: function (state) {
+            state.counter.count--;
+        },
+        INCR_COUNTER_BY: function (state, payload) {
+            state.counter.count += payload.value;
+        }
     },
     actions: {}
 }
