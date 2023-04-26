eb998c7e032b3ed9e1ca7447e859fd6d61131643
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-26 15:23:06 +0530

    Employee data property added

--- src/components/EmployeeVue.vue
+++ src/components/EmployeeVue.vue
@@ -1,13 +1,69 @@
 <template>
-    <div>
-        <p>Employee</p>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <p class="h3 text-success">Employees</p>
+                <p class="fst-italic">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae ducimus voluptas
+                    laudantium illo dolorum, quisquam vel aut odio fuga mollitia corrupti! Perferendis molestiae distinctio
+                    sit quod, tempore provident perspiciatis dolore.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    name: "EmployeeVue",
+    data: function () {
+        return {
+            employees: [
+                {
+                    "id": 1,
+                    "name": "Leanne Graham",
+                    "username": "Bret",
+                    "email": "Sincere@april.biz",
+                    isSelected: false
+                },
+                {
+                    "id": 2,
+                    "name": "Ervin Howell",
+                    "username": "Antonette",
+                    "email": "Shanna@melissa.tv",
+                    isSelected: false
+                },
+                {
+                    "id": 3,
+                    "name": "Clementine Bauch",
+                    "username": "Samantha",
+                    "email": "Nathan@yesenia.net",
+                    isSelected: false
+                },
+                {
+                    "id": 4,
+                    "name": "Patricia Lebsack",
+                    "username": "Karianne",
+                    "email": "Julianne.OConner@kory.org",
+                    isSelected: false
+                },
+                {
+                    "id": 5,
+                    "name": "Chelsey Dietrich",
+                    "username": "Kamren",
+                    "email": "Lucio_Hettinger@annie.ca",
+                    isSelected: false
+                }
+
+            ]
+
+        }
+    }
 }
 </script>
-<style>
-    
-</style>
+<style></style>
