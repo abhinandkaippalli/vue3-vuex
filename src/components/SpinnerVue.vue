48e59868d11467b4d01ba945ce26b66e963278e5
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-27 15:49:29 +0530

    loading gif component created

---
+++ src/components/SpinnerVue.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <img src="../assets/spinner.gif" alt="" class="d-block m-auto">
+    </div>
+</template>
+<script>
+export default {
+    name: "spinnerVue", 
+}
+</script>
+<style lang="">
+    
+</style>
