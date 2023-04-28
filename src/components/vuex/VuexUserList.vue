50016de85908fdb94f40ac538f3cc73f3037af0d
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-28 09:44:07 +0530

    small changes

--- src/components/vuex/VuexUserList.vue
+++ src/components/vuex/VuexUserList.vue
@@ -1,37 +1,37 @@
 <template>
-    <div class="container mt-3">
-      <div class="row">
-        <div class="col">
-          <p class="h3 text-success">User List</p>
-          <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquam aliquid aperiam
-            dignissimos dolor
-            doloribus dolorum est facere ipsam, laborum molestiae molestias numquam, quis tempore ut, veritatis voluptates
-            voluptatum.</p>
-        </div>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-success">User List</p>
+        <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquam aliquid aperiam
+          dignissimos dolor
+          doloribus dolorum est facere ipsam, laborum molestiae molestias numquam, quis tempore ut, veritatis voluptates
+          voluptatum.</p>
       </div>
     </div>
-  
-    <div v-if="userState.loading" class="container">
-      <div class="row">
-        <div class="col">
-          <Spinner/>
-        </div>
+  </div>
+
+  <div v-if="userState.loading" class="container">
+    <div class="row">
+      <div class="col">
+        <SpinnerVue />
       </div>
     </div>
-  
-    <div v-if="!userState.loading && userState.errorMessage" class="container">
-      <div class="row">
-        <div class="col">
-          <p class="fw-bold text-danger">{{ userState.errorMessage }}</p>
-        </div>
+  </div>
+
+  <div v-if="!userState.loading && userState.errorMessage" class="container">
+    <div class="row">
+      <div class="col">
+        <p class="fw-bold text-danger">{{ userState.errorMessage }}</p>
       </div>
     </div>
-  
-    <div v-if="!userState.loading && userState.users.length > 0" class="container">
-      <div class="row">
-        <div class="col">
-          <table class="table table-hover text-center table-striped">
-            <thead class="bg-secondary text-white">
+  </div>
+
+  <div v-if="!userState.loading && userState.users.length > 0" class="container">
+    <div class="row">
+      <div class="col">
+        <table class="table table-hover text-center table-striped">
+          <thead class="bg-secondary text-white">
             <tr>
               <th>SNO</th>
               <th>Name</th>
@@ -40,8 +40,8 @@
               <th>Company</th>
               <th>Location</th>
             </tr>
-            </thead>
-            <tbody>
+          </thead>
+          <tbody>
             <tr v-for="user in userState.users" :key="user.id">
               <td>{{ user.id }}</td>
               <td>{{ user.name }}</td>
@@ -50,27 +50,27 @@
               <td>{{ user.company.name }}</td>
               <td>{{ user.address.city }}</td>
             </tr>
-            </tbody>
-          </table>
-        </div>
+          </tbody>
+        </table>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  import Spinner from "../SpinnerVue.vue";
-  import {mapGetters} from "vuex";
-  export default {
-    name: "VuexUserList",
-    components: {Spinner},
-    created: function () {
-      this.$store.dispatch("usersModule/getUsers");
-    },
-    computed: mapGetters({
-      userState: "getUsersState"
-    })
-  }
-  </script>
+<script>
+import SpinnerVue from "../SpinnerVue.vue";
+
+import { mapGetters } from "vuex";
+export default {
+  name: "VuexUserList",
+  components: { SpinnerVue },
+  created: function () {
+    this.$store.dispatch("usersModule/getUsers");
+  },
+  computed: mapGetters({
+    userState: "getUsersState"
+  })
+}
+</script>
   
-  <style scoped>
-  </style>
+<style scoped></style>
